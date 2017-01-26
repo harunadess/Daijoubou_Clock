3d71783292ae894a0e4b658ddd62007bd5dca337
jordanjohnston <jjohnston498@qub.ac.uk>
2017-01-26 18:40:33 +0000

    +bodged new cancel alarm procedure +hopefully plays time applicable sound, rather than repeating

--- app/src/main/java/thel0neshadow/daijoubou_clock/RingtonePlayingService.java
+++ app/src/main/java/thel0neshadow/daijoubou_clock/RingtonePlayingService.java
@@ -5,22 +5,12 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.icu.text.DateFormat;
 import android.media.MediaPlayer;
 import android.os.Build;
-import android.os.Environment;
 import android.os.IBinder;
-import android.util.Log;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Date;
 
 /**
